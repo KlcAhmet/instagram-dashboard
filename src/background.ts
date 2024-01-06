bf6d458898f35622eea47e5fd38b52bd791029f5
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2024-01-06 04:20:45 +0300

    Update storege

--- src/background.ts
+++
@@ -1,5 +0,0 @@
-import { index, persistor } from "~store"
-
-persistor.subscribe(() => {
-  console.log("State changed with: ", index?.getState())
-})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,27 @@
-import { StrictMode, useState } from "react"
+import { useState } from "react"
 import { Provider } from "react-redux"
 
 import "~indexedDB"
 
-import { PersistGate } from "@plasmohq/redux-persist/integration/react"
+import { store } from "src/store"
 
 import { Login } from "~components/login"
 import { Profile } from "~components/profile"
-import { index, persistor } from "~store"
 
-import Icon from "../../assets/icon.png"
-import XIcon from "../appassets/XIcon.png"
+
+
 
 
 function IndexPage() {
   const [activeProfile, setActiveProfile] = useState(false)
 
   return (
-    <StrictMode>
-      <Provider store={index}>
-        <PersistGate loading={null} persistor={persistor}>
-          <div className="absolute inline bg-gray rounded-lg p-2 top-[70px] left-5">
-            <div className="flex justify-end">
-              <button
-                className="w-6 h-6"
-                onClick={() => setActiveProfile(!activeProfile)}>
-                {activeProfile ? (
-                  <img src={`${XIcon}`} className="w-full h-full" alt="Xicon" />
-                ) : (
-                  <img src={`${Icon}`} className="w-full h-full" alt="Iicon" />
-                )}
-              </button>
-            </div>
-            {activeProfile ? (
-              <div className="min-w-[400px] max-h-[750px]">
-                <Login />
-                <Profile />
-              </div>
-            ) : null}
-          </div>
-        </PersistGate>
-      </Provider>
-    </StrictMode>
+    <Provider store={store}>
+      <div className="bg-black text-white w-screen h-screen">
+        <Login />
+        <Profile />
+      </div>
+    </Provider>
   )
 }
 
