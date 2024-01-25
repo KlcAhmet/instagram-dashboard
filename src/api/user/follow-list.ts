0edfb96f5841fbd5acda396d3f9b628c44e7d1cb
Ahmet Batuhan Kılıç <ahmetbatukilic@gmail.com>
2024-01-25 15:47:30 +0300

    Code Refactor on profile.ts - return value

--- src/api/user/follow-list.ts
+++ src/api/user/follow-list.ts
@@ -1,15 +1,28 @@
-import type { TFollowersList, TUserList } from "src/types"
+import type { TFollowersList } from "src/types"
 
 import config from "~config.json"
+import HttpStatusCode from "~enums/http-status-codes"
 import { getAllCookies } from "~helpers"
 
 
 
 
 
+async function urlToBase64(url: string): Promise<string> {
+  const response = await fetch(url)
+  const blob = await response.blob()
+  return await new Promise((callback) => {
+    let reader = new FileReader()
+    reader.onload = function () {
+      callback(this.result as string)
+    }
+    reader.readAsDataURL(blob)
+  })
+}
+
 export async function getFollowerList(
   maxId: string
-): Promise<TFollowersList | {}> {
+): Promise<Partial<TFollowersList> | HttpStatusCode> {
   const cookies = getAllCookies()
   let headers = new Headers()
 
@@ -32,18 +45,30 @@ export async function getFollowerList(
     }/followers/?count=${23}`
 
   const response = await fetch(url, requestOptions)
-    .then((response) => response.json())
-    .then((result) => result)
+    .then((response) => {
+      if (response.status !== HttpStatusCode.OK_200) return response.status
+
+      return response.json()
+    })
     .catch((error) => console.log("error", error))
 
-  const {
-    next_max_id,
-    users
-  }: { next_max_id: string; users: Array<TUserList> } = await response
+  const { next_max_id, users }: Partial<TFollowersList> = await response
+
+  const usersBase64Images = await Promise.all(
+    users.map(async (item) => {
+      const imageUrl = item.profile_pic_url
+
+      const base64ImgSrc = await urlToBase64(imageUrl)
+      return {
+        ...item,
+        profile_pic_url: base64ImgSrc
+      }
+    })
+  )
 
   return {
-    next_max_id: next_max_id,
-    users: users
+    next_max_id,
+    users: usersBase64Images
   }
 }
 
@@ -76,10 +101,7 @@ export async function getFollowingList(
     .then((result) => result)
     .catch((error) => console.log("error", error))
 
-  const {
-    next_max_id,
-    users
-  }: { next_max_id: string; users: Array<TUserList> } = await response
+  const { next_max_id, users }: Partial<TFollowersList> = await response
 
   return {
     next_max_id: next_max_id,
